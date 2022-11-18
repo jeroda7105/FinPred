185c628b503677488f6bcf7000505499edca0b86
Jose Rodriguez-Acosta <jeroda7105@tamu.edu>
2022-11-18 11:11:22 -0600

    Added more skeletons and noted dependencies for functions be implemented

--- R/functions.R
+++ R/functions.R
@@ -13,15 +13,36 @@ time_summary <- function(data){
 
 # Takes in multiple time series and produces a graph
 # of their correlations
+# NOTE: Requires igraph package
 cor_graph <- function(data){
   return(data)
 }
 
 
 # Performs model selection for time series data using a rolling window
-# for the arma model and outputs this model
-arma_cv <- function(data){
+# for arma and outputs this model
+arma_selection <- function(data){
   return(data)
 }
 
+# Performs model selection for time series data using a rolling window
+# for random forests and outputs this model
+# NOTE: Requires randomForest package
+rf_selection <- function(data){
+  return(data)
+}
+
+# Performs model selection for time series data using a rolling window
+# for Support Vector Regression and outputs this model
+# NOTE: Requires e1071 package
+svr_selection <- function(data){
+  return(data)
+}
+
+# Performs model selection for time series data using a rolling window
+# for xgboost and outputs this model
+# NOTE: Requires xgboost package
+xgboost_selection <- function(data){
+  return(data)
+}
 
