6c8d2031f72bdabb1252b882fe794bab050e890a
Jose Rodriguez-Acosta <jeroda7105@tamu.edu>
2022-11-17 23:50:43 -0600

    Added skeletons for some of the functions to be implemented

---
+++ R/functions.R
@@ -0,0 +1,27 @@
+
+# Takes in a vector of price data and converts it to returns
+prices_to_returns <- function(data){
+  return(data)
+}
+
+# takes in a single or multiple time series of returns and
+# outputs a table with summary statistics for the time series
+time_summary <- function(data){
+  return(data)
+}
+
+
+# Takes in multiple time series and produces a graph
+# of their correlations
+cor_graph <- function(data){
+  return(data)
+}
+
+
+# Performs model selection for time series data using a rolling window
+# for the arma model and outputs this model
+arma_cv <- function(data){
+  return(data)
+}
+
+
